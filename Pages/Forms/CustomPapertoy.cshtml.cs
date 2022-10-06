8caf65546a8a48204efa59ab3f4afd54b7d9bc58
Alex Kanakis <lakanakis@gmail.com>
2022-10-07 02:34:55 +0300

    Add project files.

---
+++ Pages/Forms/CustomPapertoy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPapertoy.Pages.Forms
+{
+    public class CustomPapertoyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Papertoy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPapertoy.Pages
+{
+    public class PapertoyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
